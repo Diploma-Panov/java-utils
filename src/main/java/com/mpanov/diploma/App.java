0542497a38def72758cb3415ea0ebd170ecbcafd
Maksym Panov <work.maksym.panov@gmail.com>
2025-03-17 09:36:22 +0200

    Genesis

---
+++ src/main/java/com/mpanov/diploma/App.java
@@ -0,0 +1,13 @@
+package com.mpanov.diploma;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
